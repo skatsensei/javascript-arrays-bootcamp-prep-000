09b2ba7c491cd8234f6504ba01fe907a0a154281
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-11-26 07:25:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array
 }
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.lenght - 1)
+  array.slice(0, array.length - 1)
   return array
 }
