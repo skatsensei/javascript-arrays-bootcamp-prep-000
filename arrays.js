f04e022f89073a3b6e7feafb97b6d57bfe43d2c5
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-11-26 06:53:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 function addElementToEndOfArray(array, element){
   return[...array,element]
 }
+function destructivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+  return array
+}
